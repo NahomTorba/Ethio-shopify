5fd229103d7acbb8953857cc7856c9e8687e0fff
Your Name <you@example.com>
2026-05-15 16:28:55 +0300

    additoinal-frontend-link

--- backend/ethio_shopify_api/config/initializers/cors.rb
+++ backend/ethio_shopify_api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://ethio-shopify-chi.vercel.app", "http://localhost:5173"
+    origins "https://ethio-shopify-chi.vercel.app", "http://localhost:5173",  "https://web.telegram.org", "*"
 
     resource "*",
       headers: :any,
